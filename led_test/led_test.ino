a3db4e375f628ae16590b2cd48c4759efe970761
unknown <DAWells>
2022-02-12 00:23:13 +0000

    Sin wave brightness

--- led_test/led_test.ino
+++ led_test/led_test.ino
@@ -12,7 +12,8 @@ void setup() {
 }
 void loop(){
   // hue_flood(10, 50);
-  light_run(250, random8(), random16());
+  // light_run(250, random8(), random16());
+  wave(5, 100, 72);
   // int hue=random8();
   // bright_cycle(10, 100, hue);
   // gamma_fade(hue);
